90e0c9eeca2c0dcd539d11d8b8160ff2c591b376
Johnny <110586185+JonR-lang@users.noreply.github.com>
2023-09-17 17:44:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const navList = document.querySelector('.nav-list');
 
 menuIcon.addEventListener('click', () => {
     navList.classList.add('active');
-    document.body.style.overflow = 'hidden';
     setTimeout(() => {
         document.body.style.background = 'rgba(0, 0, 0, 0.2)';
     }, 300)
@@ -14,6 +13,5 @@ menuIcon.addEventListener('click', () => {
 
 closeIcon.addEventListener('click', () => {
     navList.classList.remove('active');
-    document.body.style.overflow = 'auto';
     document.body.style.background = 'white';
 })
